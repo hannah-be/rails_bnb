b604759f46483edfa9616d5ffa51b82f07430eda
Hannah Bourke <hannah25@gmail.com>
2017-10-12 10:54:48 +1100

    Add map and seed database

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,8 @@
 module ListingsHelper
+# Takes 10000 cents and outputs $100.00
+  def format_cents(cents)
+    money = Money.new(cents, 'AUD')
+    money.format
+  end
+
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,16 @@
 class Listing < ApplicationRecord
+  validates :title, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :country_code, presence: true
+  validates :country, presence: {
+    message: -> (listing, data) { "No country for code: #{listing.country_code}" }
+  }
+
+  validates :bed_count, numericality: { greater_than: 0 }
+  validates :bedroom_count, numericality: { greater_than: 0 }
+  validates :bathroom_count, numericality: { greater_than: 0 }
+
   geocoded_by :full_address
   after_validation :geocode
   monetize :night_fee_cents
@@ -10,6 +22,7 @@ class Listing < ApplicationRecord
 
   def full_address
     # [street_address, city, country.name].join(',')
+    return nil if country.nil?
     "#{street_address}, #{city}, #{country.name}"
   end
 
